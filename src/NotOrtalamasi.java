8a037ac17093a3a208b81c99c44fa9ed6d3d8a72
Kemal Songur <kemal5105@gmail.com>
2021-12-17 23:34:42 +0300

    Versiyon 1.1

--- src/NotOrtalamasi.java
+++ src/NotOrtalamasi.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class NotOrtalamasi {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the physic note: ");
