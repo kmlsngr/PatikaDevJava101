fd790d487ba47c361272943fab5921d49cb7f287
Kemal Songur <kemal5105@gmail.com>
2021-12-22 12:45:42 +0300

    Versiyon 3.5

---
+++ src/ExponentialNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ExponentialNumber {
+
+    static int exponentialNum(int base,int exp){
+
+        if(base==0 && exp==0){
+            System.out.println("Tanımsız işlem!");
+            return 0;
+        }
+        if(exp==0){
+            return 1;
+        }
+
+        return base*exponentialNum(base,exp-1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the base: ");
+        int base = scanner.nextInt();
+        System.out.print("Enter the exponent: ");
+        int exponent = scanner.nextInt();
+        System.out.println(exponentialNum(base,exponent));
+
+    }
+}
