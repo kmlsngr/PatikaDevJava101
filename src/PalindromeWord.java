c25e3ec03e751e5282065dcd504359817e67da1c
Kemal Songur <kemal5105@gmail.com>
2021-12-27 19:34:57 +0300

    Versiyon 4.5

---
+++ src/PalindromeWord.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class PalindromeWord {
+
+
+    static boolean isPalindrome(String s){
+
+        if((s.length())%2==0){
+            int half = s.length()/2;
+            int reverse = s.length()-1;
+            for(int i = 0;i<half;i++){
+                if(s.charAt(i)==s.charAt(reverse)){
+                    reverse--;
+
+                }
+                else {
+
+                    return false;
+                }
+            }
+        }else {
+            int half = s.length()/2;
+            int reverse = s.length()-1;
+            for(int i = 0;i<half;i++){
+                if(s.charAt(i)==s.charAt(reverse)){
+                    reverse--;
+
+                }
+                else {
+
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the sentence to see it whether Palindrome: ");
+        String s = scanner.next();
+        if(isPalindrome(s)){
+            System.out.println(s+" is Palindrome!");
+        }
+        else {
+            System.out.println(s+" is not Palindrome");
+        }
+
+    }
+}
